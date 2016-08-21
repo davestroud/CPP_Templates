2c82f885a31b7cd75709030fe733ca605d79129f
davestroud <david@davidstroud.me>
2016-08-21 15:33:37 -0500

    test file functions properly

--- Pointers/test.cpp
+++ Pointers/test.cpp
@@ -19,58 +19,52 @@ int main()
 {
   double *grades = nullptr,       // To dynamically allocate an array
           total = 0.0,            // Accumlator
+          average;                // To hold the grade average
   int numGrades,                  // To hold the number of grades
       count;                      // Counter variable
-}
+
 
 // Get the number of test scores
-
-
-
-
+cout << "How many test scores do you need to process? ";
+cin >> numGrades;
 
 // Do not accept a negative number of test scores
-
-
-
-
 
 
 // Do not accept a negative number for a test score
 
 
-
-
-
 // Dynamically allocate an array large enough
 // to hold the test scores
-
-
-
-
-
+grades = new double[numGrades];
 
 // Get the actual test scores
+cout << "Enter the test scores below.\n";
+for (count = 0; count < numGrades; count++)
+{
+  cout << "Grade" << (count + 1) << ":";
+  cin >> grades[count];
+}
 
-
-
-
-
-
+//  Calculate the total scores
+for (count = 0; count < numGrades; count++)
+{
+  total += grades[count];
+}
 
 // Calculate the average score
-
-
-
-
-
+average = total / numGrades;
 
 // Display the results
-
-
-
-
-
+cout << fixed << showpoint << setprecision(2);
+cout << "Average Grade: " << average <<endl;
 
 
 // Free dynamically allocated memory
+delete [] grades;
+grades = nullptr;     // Makes grades a null pointer
+
+
+return 0;
+
+}
