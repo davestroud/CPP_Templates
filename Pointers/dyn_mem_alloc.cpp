67f5987306efaa077f9bb2818ccf4cbc9f44be97
davestroud <david@davidstroud.me>
2016-08-21 14:45:03 -0500

    finished dynamic memory allocation algorithm

--- Pointers/dyn_mem_alloc.cpp
+++ Pointers/dyn_mem_alloc.cpp
@@ -19,38 +19,34 @@ cin >> numDays;
 
 // Dynamically allocate an array large enough
 // to hold that many days of sales amounts
-sales - new double[numDays];
+sales = new double[numDays];
 
 // Get the sales figures for each day
 cout << "Enter the sales figures below.\n";
 for (count = 0; count < numDays; count++)
 {
-  total += sales[count];
+  cout << "Day" << (count + 1) << ": ";
+  cin >> sales[count];
 }
 
 // Calculate the the total sales
-
-
-
-
+for (count = 0; count < numDays; count++)
+{
+  total += sales[count];
+}
 
 //Calculate the average sales per day
-
-
-
-
-
+average = total / numDays;
 
 // Display the results
-
-
-
-
-
+cout << fixed << showpoint << setprecision(2);
+cout << "\n\nTotal Sales: $" << total << endl;
+cout << "Average Sales: $ " << average << endl;
 
 // Free dynamically allocated memeory
+delete [] sales;
+sales = nullptr;      // Makes sales a null pointer
 
-
-
+return 0;
 
 }
