7af43333388b3dc3b90a8567af1b8f014748c290
davestroud <david@davidstroud.me>
2016-08-21 14:14:58 -0500

    dynamically add an array and declare variables

--- Pointers/dyn_mem_alloc.cpp
+++ Pointers/dyn_mem_alloc.cpp
@@ -1,10 +1,24 @@
+// This program totals and averages the sales figures for any
+// number of days.  The figures are stored dynamically
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  double *sales = nullptr,    // To dynamically allocate an array
+          total = 0.0,        // Accumlator
+          average;            // To hold average sales
+  int numDays,                // To hold the number of days of sales
+      count;                  // Counter variable
+          
 // Get the number of days of sales
 
 
 
 
 
-// Dynamically allocate and array large enough
+// Dynamically allocate an array large enough
 // to hold that many days of sales amounts
 
 
@@ -41,3 +55,8 @@
 
 
 // Free dynamically allocated memeory
+
+
+
+
+}
---
+++ Pointers/test.cpp
@@ -0,0 +1,51 @@
+// Get the number of test scores
+
+
+
+
+
+// Do not accept a negative number of test scores
+
+
+
+
+
+
+// Do not accept a negative number for a test score
+
+
+
+
+
+// Dynamically allocate an array large enough
+// to hold the test scores
+
+
+
+
+
+
+// Get the actual test scores
+
+
+
+
+
+
+
+// Calculate the average score
+
+
+
+
+
+
+// Display the results
+
+
+
+
+
+
+
+// Free dynamically allocated memory
