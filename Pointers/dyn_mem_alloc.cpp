13383d99af81e454b9630251611ede83444ce644
davestroud <david@davidstroud.me>
2016-08-21 10:10:32 -0500

    add framework with comments to build dynamic memory allocation algorithm

---
+++ Pointers/dyn_mem_alloc.cpp
@@ -0,0 +1,43 @@
+// Get the number of days of sales
+
+
+
+
+
+// Dynamically allocate and array large enough
+// to hold that many days of sales amounts
+
+
+
+
+
+
+// Get the sales figures for each day
+
+
+
+
+
+
+
+// Calculate the the total sales
+
+
+
+
+
+//Calculate the average sales per day
+
+
+
+
+
+
+// Display the results
+
+
+
+
+
+
+// Free dynamically allocated memeory
