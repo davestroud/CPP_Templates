e020c94ffc53ccd49ca9ecb027ec4a29df9dc32c
davestroud <david@davidstroud.me>
2016-08-21 14:56:42 -0500

    dynamically add array and add other variables

--- Pointers/test.cpp
+++ Pointers/test.cpp
@@ -1,3 +1,28 @@
+// John David Stroud
+// CS2255 Computer Science II
+// david@davidstroud.me
+// https://github.com/davestroud
+
+/* This program dynamically allocates an array large enough to hold a user
+    defined number of test scores.  Once all scores are entered, the array should
+    be passed to a function that sorts them in ascending order.  Another
+    funcion should be called that calculates the average score.  The program
+    should display the sorted list of scores and averages with appropriate
+    headings.  Pointer notation is used, rather than array notation whenever possible.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  double *grades = nullptr,       // To dynamically allocate an array
+          total = 0.0,            // Accumlator
+  int numGrades,                  // To hold the number of grades
+      count;                      // Counter variable
+}
+
 // Get the number of test scores
 
 
