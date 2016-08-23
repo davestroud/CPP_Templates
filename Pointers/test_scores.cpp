876e612d8655a4757cf6b5d935a8a0bb6ef8ad43
davestroud <david@davidstroud.me>
2016-08-23 12:42:51 -0500

    add function prototypes

---
+++ Pointers/test_scores.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+
+int main()
+{
+  float* grades = nullptr;      // To dynamically allocate an array
+  float numGrades;              // To hold the number of grades
+}
+
+// Get the number of test scores
+
+
+
+
+
+// Do not accept a negative number of test scores
+
+
+
+
+
+
+// Do not accept a negative number for a test score
+
+
+
+
+
+
+// Dynamically allocate an array large enough
+// to hold the test scores
+grades = new float[numGrades];
+
+
+
+
+
+// Get the actual test scores
+
+
+
+
+
+
+// Calculate the total scores
+
+
+
+
+
+// Calculate the average score
+
+
+
+
+
+// Display the results
+
+
+
+
+// Free dynamically allocated memory
+delete [] grades;
+grades = nullptr;   // Makes grades a null pointer 
+
+
+
+// Sorting algorithm
