56847f1d2cf4ce13d0356d3fafa023d1b4ed5517
davestroud <david@davidstroud.me>
2016-08-24 16:19:38 -0500

    add password verifier file and comments as an outline

---
+++ Chapter_10/password_verifier.cpp
@@ -0,0 +1,17 @@
+// Your password should be at least six characters long
+
+
+
+// Your password should contain at least one uppercase and
+// at least one lowercase letter
+
+
+
+
+
+// The password should have one digit
+
+
+
+
+// The password is not valid
