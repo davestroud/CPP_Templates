8c11225c467e13809c6e6ebc50c89dec91632c1c
davestroud <david@davidstroud.me>
2016-08-25 12:58:53 -0500

    add file that assumes that two strings are the same class object

---
+++ theSame.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio> // For the C getchar() function
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << endl << "Press any key to continue..." << endl;
+  getchar();
+
+  return 0;
+}
