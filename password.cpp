e60efab4d34ed6f67da7f25e43e7a1eec83a54ad
davestroud <david@davidstroud.me>
2016-08-26 13:05:55 -0500

    laid out comments framework for the algorithm

---
+++ password.cpp
@@ -0,0 +1,19 @@
+// Enter a password
+
+
+// Password must have at least 6 characters
+
+
+// Password must have at least one uppercase character
+
+
+// Password must have at lease one lowercase character
+
+
+// Password must have at least one numeric digit
+
+
+// Your password is valid
+
+
+// Your password is not valid
