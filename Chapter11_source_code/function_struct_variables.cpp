a4f2b0c1f64175bf9adfbda17ad44c2525337dfc
davestroud <david@davidstroud.me>
2016-08-31 17:19:46 -0500

    program complete

--- Chapter11_source_code/function_struct_variables.cpp
+++ Chapter11_source_code/function_struct_variables.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 struct InventoryItem
 {
-  int partNum;            // Part number
-  string description      // Item description
-  int onHand;             // Units on hand
-  double price;           // Unit price
+  int partNum;              // Part number
+  string description;       // Item description
+  int onHand;               // Units on hand
+  double price;             // Unit price
 };
 
 // Function Prototypes
@@ -32,11 +32,37 @@ int main()
 // the user for information to store in its structure        *
 //************************************************************
 
+void getItem(InventoryItem &p)    // Uses a reference parameter
+{
+  // Get the part number
+  cout << "Enter the part number: ";
+  cin >> p.partNum;
 
+  // Get the part description
+  cout << "Enter the part description: ";
+  cin.ignore(); // Ignore the remaining newline character
+  getline(cin, p.description);
 
+  // Get the quantity on hand
+  cout << "Enter the quantity on hand: ";
+  cin >> p.onHand;
+
+  // Get the unit price
+  cout << "Enter the unit price: ";
+  cin >> p.price;
+}
 
 //***********************************************************
 //Definition of function showItem.  This function accepts   *
 //an argument of the InventoryItem structure type.  The     *
 // contents of the structure is displayed.                  *
 //************************************************************
+
+void showItem(InventoryItem p)
+{
+  cout << fixed << showpoint << setprecision(2);
+  cout << "Part Number: " << p.partNum << endl;
+  cout << "Description: " << p.description << endl;
+  cout << "Units on Hand: " << p.onHand << endl;
+  cout << "Price: $" << p.price << endl;
+}
