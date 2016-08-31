c16276bc54de759c0378a6d74139c0e050a8d4fc
davestroud <david@davidstroud.me>
2016-08-31 17:50:41 -0500

    add comments to modified function structure file

---
+++ Chapter11_source_code/modified_function_structure.cpp
@@ -0,0 +1,20 @@
+// This program uses a function to return a structure.  This
+// is a modification of circle_struct.cpp
+
+//Constant for Pi
+
+// Structure declaration
+
+
+// Function prototype
+
+
+
+//*****************************************************************
+// Definition of function getInfo.  This function uses a local    *
+// variable, tempCircle, which is a circle structure.  The user   *
+// enter the diameter of the circle which is stored in            *
+// tempCircle.diameter.  The function then calculates the radius  *
+// which is stored in tempCircle.radius. tempCircle is then       *
+// return from the function.                                      *
+//*****************************************************************
