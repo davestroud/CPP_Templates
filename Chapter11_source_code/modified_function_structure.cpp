227496a878ede86d15b61a589d493e23d79c2931
davestroud <david@davidstroud.me>
2016-08-31 17:57:30 -0500

    add main function, function prototype and structure declaration

--- Chapter11_source_code/modified_function_structure.cpp
+++ Chapter11_source_code/modified_function_structure.cpp
@@ -1,13 +1,39 @@
 // This program uses a function to return a structure.  This
 // is a modification of circle_struct.cpp
+#include <iostream>
+#include <iomanip>
+#include <math>   // For the pow function
+using namespace std;
 
 //Constant for Pi
+const double PI = 3.14159;
 
 // Structure declaration
-
+struct Circle
+{
+  double radius;        // A circles radius
+  double diameter;      // A circles diameter
+  double area;          // A circles area
+};
 
 // Function prototype
+Circle getInfo();
 
+int main()
+{
+  Circle c;       // Define a structure variable
+
+  // Get data about the circle
+  c.area = PI * pow(c.radius, 2.0);
+
+
+  // Display the circle data
+  cout << "The radius and area of the circle are:\n";
+  cout << fixed << setprecision(2);
+  cout << "Radius: " << c.radius << endl;
+  cout << "Area " << c.area << endl;
+  return 0;
+}
 
 
 //*****************************************************************
