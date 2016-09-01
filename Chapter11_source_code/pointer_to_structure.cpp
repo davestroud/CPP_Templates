a463e1a2a5502bd9e505d01709b81cdfea285130
davestroud <david@davidstroud.me>
2016-09-01 14:34:18 -0500

    add comments and build struct

---
+++ Chapter11_source_code/pointer_to_structure.cpp
@@ -0,0 +1,18 @@
+// This program demonstrates a function that uses a
+// pointer to a structure variable as a parameter
+
+// Build struct
+struct Student
+{
+  string name;            // Students name
+  int idNum;              // Student ID number
+  int creditHours;        // Credit hours enrolled
+  double gpa;             // Current GPA 
+}
+
+
+//***********************************************************
+// Definition of function getData. Uses a pointer to a      *
+// Student structure variable. The user enters student      *
+// information, which is stored in the variable.            *
+//***********************************************************
