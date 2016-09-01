0d3776472fdd2c7762602a3b2ec899106b307a8e
davestroud <david@davidstroud.me>
2016-09-01 14:44:57 -0500

    add main function

--- Chapter11_source_code/pointer_to_structure.cpp
+++ Chapter11_source_code/pointer_to_structure.cpp
@@ -7,9 +7,28 @@ struct Student
   string name;            // Students name
   int idNum;              // Student ID number
   int creditHours;        // Credit hours enrolled
-  double gpa;             // Current GPA 
+  double gpa;             // Current GPA
 }
 
+void getData(Student *);    // Function prototype
+
+int main()
+{
+  Student freshman;
+
+  // Get the student data
+  cout << "Enter the folowing student data:\n";
+  getData(&freshman);       // Pass the address of freshman
+  cout << "\nHere is the student data you entered:\n";
+
+  // Now display the data stored in freshman
+  cout << setprecision(3);
+  cout << "Name: " << freshman.name << endl;
+  cout << "ID Number: " << freshman.idNum << endl;
+  cout << "Credit Hours: " << freshman.creditHours << endl;
+  cout << "GPA: " << freshman.gpa << endl;
+  return 0;
+}
 
 //***********************************************************
 // Definition of function getData. Uses a pointer to a      *
