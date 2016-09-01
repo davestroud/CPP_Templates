d99b359e462a57045115650281008c529dadbe08
davestroud <david@davidstroud.me>
2016-09-01 16:54:13 -0500

    add union.cpp file and comments for the file

---
+++ Chapter11_source_code/union.cpp
@@ -0,0 +1,37 @@
+// This program demonstrates a union
+
+
+
+  // Get the pay type, hourly or commission
+
+
+
+  // Determine the gross pay, depending on the pay type
+
+
+
+
+    //This is an hourly paid employee. Get the
+    // pay rate and hours worked
+
+
+
+
+
+    // Calculate and display the gross pay.
+
+
+
+
+
+    // This is a commission-paid employee. Get the
+    // amout of sales.
+
+
+
+    // Calculate and display the gross pay.
+
+
+
+
+    // This user made an invalid selection.
