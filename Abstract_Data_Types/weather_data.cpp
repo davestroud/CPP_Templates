4667c7fd71d9e10909bae6eb43857037834c1e2a
davestroud <david@davidstroud.me>
2016-09-03 19:17:12 -0500

    add struct for weather_data file

---
+++ Abstract_Data_Types/weather_data.cpp
@@ -0,0 +1,16 @@
+// Write a program that uses structure to store the following weather data
+// for a particular month:
+
+// Total Rainfall
+// High Temperature
+// Low Temperature
+// Average Temperature
+
+// Build struct
+struct WeatherData
+{
+  double rain;          // Total Rainfall
+  double high;          // High Temperature
+  double low;           // Low Temperature
+  double averageTemp;   // Average Temperature
+};
