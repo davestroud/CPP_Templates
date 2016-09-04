0292655b2dfbedd21480811d61cd694935b5141a
davestroud <david@davidstroud.me>
2016-09-04 17:49:40 -0500

    add enum_data_type file

---
+++ Abstract_Data_Types/enum_data_type.cpp
@@ -0,0 +1,30 @@
+// This program demonstrates an enumerated data type.
+#include <iostream>
+#include <iomanip>
+
+enum Day { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY };
+
+int main(int argc, char const *argv[]) {
+  cont int NUM_DAYS = 5       // The number of days
+  double sale[NUM_DAYS];      // To hold sales for each days
+  double total = 0.0;         // Accumulator
+  int index;                  // Loop counter
+
+  // Get the sales for each day
+  for (index = MONDAY; index <= FRIDAY; index++)
+  {
+    std::cout << "Enter the sales for day " << std::endl;
+              << index << ": ";
+          std::cin >> sales[index];
+  }
+
+  // Calculate the total sales
+  for (index = MONDAY; index <= FRIDAY; index++)
+    total += sales[index];
+
+  // Display the total
+  std::cout << "The total sales are $" << setprecision(2) std::endl;
+            << fixed << total << endl;
+
+  return 0;
+}
