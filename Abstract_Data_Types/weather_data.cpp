ebdda89635b8f08c83daccc3b64456a815a3c4b5
davestroud <david@davidstroud.me>
2016-09-03 20:38:11 -0500

    add main function to weather_data

--- Abstract_Data_Types/weather_data.cpp
+++ Abstract_Data_Types/weather_data.cpp
@@ -19,6 +19,36 @@ struct WeatherData
 void getMonthData(WeatherData &);
 double totalRain(WeatherData[], int);
 double averageMonthlyRainfall(WeatherData[], int);
-double averageAverageTemp(WeatherData{}, int);
+double averageAverageTemp(WeatherData[], int);
 double highestTemp(WeatherData[], int, int &);
 double lowestTemp(WeatherData[], int, int &);
+
+int main()
+{
+  // Constant for the number of months
+  const int NUM_MONTHS = 12;
+
+  // Create an array of WeatherData structure
+  WeatherData year[NUM_MONTHS];
+
+  int highestMonth;       // The month with the highest temp
+  int lowestMonth;        // The month with the lowest tmep
+
+  // Get the weather data for each month
+  cout << "Enter the following information:\n";
+  for (int index = 0; index < NUM_MONTHS; index++)
+  {
+      // Get the Rainfall
+      cout << "Month " << (index + 1) << endl;
+      getMonthData(year[index]);
+  }
+
+  // Display the total Rainfall
+  cout << "\nTotal Rainfall:"
+        << totalRain(year, NUM_MONTHS) << endl;
+
+  // Display the average monthly Rainfall
+  cout << "Average Monthly Rain: "
+        << averageMonthlyRainfall(year, NUM_MONTHS)
+
+}
