e7228864dce6a584aa744ff7eb9421123fb3abe7
davestroud <david@davidstroud.me>
2016-09-06 10:48:06 -0500

    add pass_by_ref file

---
+++ File_Operations/12_book/pass_by_ref.cpp
@@ -0,0 +1,58 @@
+// This program demonstrates how file stream objects may
+// be passed by reference to functions.
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Function prototypes
+bool openFileIn(fstream &, string);
+void showContents(fstream &);
+
+int main(int argc, char const *argv[]) {
+  fstream dataFile;
+
+  if (openFileIn(dataFile, "demofile.txt")) {
+    cout << "File opened successfully.\n";
+    cout << "Now reading data from the file.\n\n";
+    showContents(dataFile);
+    dataFile.close();
+    cout << "\nDone.\n";
+  }
+  else
+      cout << "File open error!" << endl;
+
+  return 0;
+}
+
+//***************************************************************
+// Definition of function openFileIn.  Accepts a reference      *
+// to an fstream object as an argument.  This file is opened    *
+// of input.  The function returns true upon success, false     *
+// upon failure.                                                *
+//***************************************************************
+
+bool openFileIn(fstream &file, string name)
+{
+  file.open(name, ios::in);
+  if (file.fail())
+      return false;
+  else
+      return true;
+}
+
+
+//*************************************************************
+// Definition of function showContents.  Accepts an fstream   *
+// reference as its arguement.  Uses a loop to read each name *
+// from the list and displays it on the screen.               *
+//*************************************************************
+
+void showContents(fstream &file)
+{
+  string line;
+
+  while (file >> line) {
+    cout << line << endl;
+  }
+}
