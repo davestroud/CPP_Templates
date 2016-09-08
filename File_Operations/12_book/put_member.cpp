8aa898d56f3c504136c1691d98866ca766af9de6
davestroud <david@davidstroud.me>
2016-09-08 10:05:59 -0500

    add put_member file

---
+++ File_Operations/12_book/put_member.cpp
@@ -0,0 +1,28 @@
+// This program demonstrates the put member function
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  char ch;      // To hold a character
+
+  // Open the file for output.
+  fstream dataFile("sentence.txt", ios::out);
+
+  cout << "Type the sentence and be sure to end it with a ";
+  cout << "period.\n";
+
+  // Get the sentence from the user one character at a time
+  // and write each character to the file.
+  cin.get(ch);
+  while (ch != '.')
+  {
+    dataFile.put(ch);
+    cin.get(ch);
+  }
+  dataFile.put(ch);   // Write the period.
+
+  // Close the file.
+  dataFile.close();
+  return 0;
+}
