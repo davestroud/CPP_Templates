a6a7d928446630ba502f9df9c98f3004485b3dea
davestroud <david@davidstroud.me>
2016-09-08 09:41:38 -0500

    add getline_delim as a file for using a delimter

---
+++ File_Operations/12_book/getline_delim.cpp
@@ -0,0 +1,39 @@
+// This program uses the getLine function to read line of
+// data from the file
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  string input;     // To hold file input
+
+  // Open the file for input
+  fstream dataFile("names2.txt", ios::in);
+
+  // If the file was successfully opened, continue.
+  if (dataFile)
+  {
+    // Read an item using $ as a delimiter.
+    getline(dataFile, input, '$');
+
+    // While the last read operation
+    // was successful, continue.
+    while (dataFile)
+    {
+      // Display the last item rad.
+      cout << input << endl;
+
+      // Read an item using $ as a delimiter.
+      getline(dataFile, input, '$');
+    }
+
+    // Close the file.
+    dataFile.close();
+  }
+  else
+  {
+    cout << "ERROR: Cannot open file.\n";
+  }
+  return 0;
+}
