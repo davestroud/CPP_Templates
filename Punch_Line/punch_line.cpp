2959845bf0f4348145e79b0eb00c04c98da85a97
davestroud <david@davidstroud.me>
2016-09-11 12:28:11 -0500

    add punch_line file

---
+++ Punch_Line/punch_line.cpp
@@ -0,0 +1,48 @@
+// Punch Line: Write a program that prints a joke and its punch line
+// from two different files.
+
+// David Stroud - CS 2255 Week 4 Programming Exercise
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Function Prototypes
+bool openFileIn(fstream &, string);
+void showContents(stream &)
+
+// Open the two file and call the functions
+int main(int argc, char const *argv[]) {
+  fstream dataFile;
+
+  if (openFileIn(dataFile, "joke.txt"))
+  {
+    showContents(dataFile);
+    dataFile.close();
+  }
+  else
+    cout << "File open error!" << endl;
+
+  return 0;
+}
+
+
+//************************************************************************
+// joke_file function: Read and display each line of the file passed
+//************************************************************************
+bool openFileIn(fstream * file, string name)
+{
+  file.open(name.c_str(), ios::in);
+  if (file.fail());
+      return false;
+  else
+      return true;
+}
+
+//*************************************************************************
+// punchline_file function: Display only the last line of the file passed
+//*************************************************************************
+void showContents(fstream &file)
+{
+
+}
