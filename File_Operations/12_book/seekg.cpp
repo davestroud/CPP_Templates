be7690c46e359f16bc7771f694ef723dee2f830f
davestroud <david@davidstroud.me>
2016-09-12 11:25:55 -0500

    add seekg file

---
+++ File_Operations/12_book/seekg.cpp
@@ -0,0 +1,32 @@
+// This program demonstrates the seekg function.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  char ch;      // To hold a character
+
+  // Open the file for input.
+  fstream file("letters.txt", ios::in);
+
+  // Move the byte 5 from the beginning of the file
+  // (the 6th byte) and read the character there.
+  file.seekg(5L, ios::beg);
+  file.get(ch);
+  cout << "Byte 5 from the beginning: " << ch << endl;
+
+  // Move the 10th byte from the end of the file
+  // and read the character there.
+  file.seekg(-10L, ios::end);
+  file.get(ch);
+  cout << "10th byte from the end: " << ch << endl;
+
+  // Move to byte 3 from the current position
+  // (the 4th byte) and read the character there.
+  file.seekg(3L, ios::cur);
+  file.get(ch);
+  cout << "Byte 3 from current: " << ch << endl;
+
+  file.close();
+  return 0;
+}
