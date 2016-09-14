981e424c23a164e68ef901b80bbb824a13105234
davestroud <david@davidstroud.me>
2016-09-14 09:40:35 -0500

    add linear_search file

---
+++ Chapter_08/linear_search.cpp
@@ -0,0 +1,52 @@
+// This program demonstrates the searchList fuction, which
+// performs a linear search on an integer array.
+#include <iostream>
+
+// Function prototype
+int searchList(const int [], int, int);
+const int SIZE = 5;
+
+int main(int argc, char const *argv[]) {
+  int tests[SIZE] = {87, 75, 98, 100, 82};
+  int results;
+
+  // Search the array for 100.
+  results = searchList(tests, SIZE, 100);
+
+  // If searchlist returned -1, then 100 was not found
+  if (results == -1)
+    std::cout << "You did not earn 100 points on any test." << std::endl;
+  else
+  {
+    // Otherwise results contains the subscript of
+    // the first 100 in the array.
+    std::cout << "You earned 100 points on test " << std::endl;
+    std::cout << (results + 1) << std::endl;
+  }
+  return 0;
+}
+
+//*****************************************************************
+// The searchList function performs a linear search on an         *
+// integer array. The array list, which has a maximum of numElems *
+// elements, is searched for the number stored in value. If the   *
+// number is found, its array subscript is returned. Otherwise,   *
+// -1 is returned indicating the value was not in the array.      *
+//*****************************************************************
+
+int searchList(const int list[], int numElems, int value)
+{
+  int index = 0;        // Used as a subscript to search array
+  int position = -1;     // To record position of search value
+  bool found = false;   // Flag to indicate if the value was found
+
+  while (index < numElems && !found) {
+    if (list[index] == value)   // If the value is found
+    {
+      found = true;             // Set the flag.
+      position = index;         // Record the value's subscript
+    }
+    index++;                    // Go to the next element
+  }
+  return position;              // Return the postion, or -1
+}
