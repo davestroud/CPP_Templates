4e338ac569f2b41bac242de06a05cb68de054f9a
davestroud <david@davidstroud.me>
2016-09-19 16:36:04 -0500

    add class instances.  set up class Rectangle

---
+++ 13_book/class_instances.cpp
@@ -0,0 +1,66 @@
+// This program creates three instances of the Rectangle class.
+#include <iostream>
+using namespace std;
+
+// Recatangle class declaration.
+class Rectangle
+{
+  private:
+    double width;
+    double length;
+  public:
+    void setWidth(double);
+    void setLength(double);
+    double getWidth() const;
+    double getLength() const;
+    double getArea() const;
+};
+
+//**************************************************
+// setWidth assigns a value to the width member.   *
+//**************************************************
+
+ void Rectangle::setWidth(double w)
+ {
+   width = w;
+ }
+
+//**************************************************
+// setLength assigns a value to the length member. *
+//**************************************************
+
+void Rectangle::setLength(double len)
+{
+  length = len;
+}
+
+//**************************************************
+// getWidth returns the value in the width member. *
+//**************************************************
+
+double Rectangle::getWidth() const
+{
+  return width;
+}
+
+//****************************************************
+// getLength returns the value in the length member. *
+//****************************************************
+
+double Rectangle::getLength() const
+{
+  return length;
+}
+
+//*****************************************************
+// getArea returns the product of width times length. *
+//*****************************************************
+
+double Rectangle::getArea() const
+{
+  return width * length;
+}
+
+//*****************************************************
+// Function main                                      *
+//*****************************************************
