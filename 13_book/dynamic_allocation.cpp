81b66707cae3f984d47272ea668b8476149cef9c
davestroud <david@davidstroud.me>
2016-09-20 10:33:34 -0500

    complete dynamic_allocation

--- 13_book/dynamic_allocation.cpp
+++ 13_book/dynamic_allocation.cpp
@@ -64,3 +64,59 @@ double Rectangle::getArea() const
 //*****************************************************
 // Function main                                      *
 //*****************************************************
+
+int main()
+{
+  double number;                  // To hold a number.
+  double totalArea;               // The total area.
+  Rectangle *kitchen = nullptr;   // To point to kitchen dimensions
+  Rectangle *bedroom = nullptr;   // To point to bedroom dimensions
+  Rectangle *den = nullptr;       // To point to den dimensions
+
+  // Dynamically allocate the objects.
+  kitchen = new Rectangle;
+  bedroom = new Rectangle;
+  den = new Rectangle;
+
+  // Get the kitchen dimensions
+  cout << "What is the kitchen's length? ";
+  cin >> number;                // Get the length
+  kitchen->setLength(number);   // Store in kitchen object
+  cout << "What is the kitchen's width? ";
+  cin >> number;                // Get the width
+  kitchen->setWidth(number);    // Store in kitchen object
+
+  // Get the bedrooms's dimensions.
+  cout << "What is the bedroom's length? ";
+  cin >> number;                // Get the length
+  bedroom->setLength(number);   // Store in bedroom object
+  cout << "What is the bedroom's width? ";
+  cin >> number;                // Get the width
+  bedroom->setWidth(number);    // Store in bedroom object
+
+  // Get the den's dimensions
+  cout << "What is the den's length? ";
+  cin >> number;                // Get the length
+  den->setLength(number);       // Store in den object
+  cout << "What is the den's width? ";
+  cin >> number;                // Get the length
+  den->setWidth(number);        // Store in den object
+
+  // Calculate the total area of the three rooms.
+  totalArea = kitchen->getArea() + bedroom->getArea()
+              + den->getArea();
+
+  // Display the total area of three rooms.
+  cout << "The total area of the three rooms is "
+        << totalArea << endl;
+
+  // Delete the objects from memory
+  delete kitchen;
+  delete bedroom;
+  delete den;
+  kitchen = nullptr;    // Make kitchen a null pointer.
+  bedroom = nullptr;    // Make bedroom a null pointer.
+  den = nullptr;        // Make den a null pointer
+
+  return 0;
+}
