11fab7ea0e9190eaefdb4755c570b66ddc8cf6b6
davestroud <david@davidstroud.me>
2016-09-20 09:38:44 -0500

    add class_instances file

--- 13_book/class_instances.cpp
+++ 13_book/class_instances.cpp
@@ -1,6 +1,5 @@
 // This program creates three instances of the Rectangle class.
 #include <iostream>
-using namespace std;
 
 // Recatangle class declaration.
 class Rectangle
@@ -64,3 +63,45 @@ double Rectangle::getArea() const
 //*****************************************************
 // Function main                                      *
 //*****************************************************
+
+int main(int argc, char const *argv[]) {
+  double number;      // To hold a number
+  double totalArea;   // The total area
+  Rectangle kitchen;  // To hold kitchen dimensions
+  Rectangle bedroom;  // To hold bedroom dimensions
+  Rectangle den;      // To hold den dimensions
+
+  // Get the kitchens dimensions
+  std::cout << "What is the kitchens length? " << std::endl;
+  std::cin >> number;                           // Get the length
+  kitchen.setLength(number);                    // Store in kitchen object
+  std::cout << "What is the kitchens width? " << std::endl;
+  std::cin >> number;                           // Get the width
+  kitchen.setWidth(number);                     // Store in kitchen object
+
+  // Get the bedrooms dimensions
+  std::cout << "What is the bedrooms length? " << std::endl;
+  std::cin >> number;                           // Get the length
+  bedroom.setLength(number);                    // Store in bedroom object
+  std::cout << "What is the bedrooms width? " << std::endl;
+  std::cin >> number;                           // Get the width
+  bedroom.setWidth(number);                     // Store in bedroom object
+
+  // Get the dens dimensions
+  std::cout << "What is the dens length? " << std::endl;
+  std::cin >> number;                           // Get the length
+  den.setLength(number);                        // Store in den object
+  std::cout << "What is the dens width? " << std::endl;
+  std::cin >> number;                           // Get the width
+  den.setWidth(number);                         // Store in den object
+
+  // Calculate the total area of the three rooms.
+  totalArea = kitchen.getArea() + bedroom.getArea()
+              + den.getArea();
+
+  // Display the total area of three rooms.
+  std::cout << "The total area of the three rooms is "
+            << totalArea << std::endl;
+
+  return 0;
+}
