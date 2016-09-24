d0a91a588f7242f46bb3244bb818ef41b9905190
davestroud <david@davidstroud.me>
2016-09-24 15:11:48 -0500

    add first employee information on Main.cpp file

--- Employee_Class/Main.cpp
+++ Employee_Class/Main.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+  Employee emp1;
+  emp1.setName("Susan Meyers");
+  emp1.setIdNumber(47899);
+  emp1.setDepartment("Accounting");
+  emp1.setPosition("Vice President");
 
+  // Display the data for emp1
+  cout << "Name: " << emp1.getName() << endl;
+  cout << "ID Number: " << emp1.getIdNumber() << endl;
+  cout << "Department: " << emp1.getDeparment() << endl;
+  cout << "Position: " << emp1.getPosition() << endl;
+
+  return 0;
 }
