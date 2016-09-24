c8d0dd17f64b67f3c0c139465b028c31437b63df
davestroud <david@davidstroud.me>
2016-09-24 10:13:23 -0500

    complete main_overload file

--- 13_book/Inventory_Item_2/main_overload.cpp
+++ 13_book/Inventory_Item_2/main_overload.cpp
@@ -16,6 +16,12 @@ int main()
   // constructor #2
   InventoryItem item2("Pliers");
 
+  // Create an InventoryItem object and call
+  // constructor #3.
+  InventoryItem item3("Wrench", 8.75, 20);
+
+  cout << "The following items are in inventory:\n";
+  cout << setprecision(2) << fixed << showpoint;
 
   // Display the data for item 1
   cout << "Description: " << item1.getDescription() << endl;
@@ -25,7 +31,11 @@ int main()
   // Display the data for item 2
   cout << "Description: " << item2.getDescription() << endl;
   cout << "Cost: " << item2.getCost() << endl;
-  cout << "Units on Hand: " <<item2.getUnits() << endl;
+  cout << "Units on Hand: " <<item2.getUnits() << endl << endl;
 
+  // Display the data for item 3
+  cout << "Description: " << item3.getDescription() << endl;
+  cout << "Cost: " << item3.getCost() << endl;
+  cout << "Units on Hand: " << item3.getUnits() << endl;
   return 0;
 }
